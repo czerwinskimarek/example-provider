3ae7e182995939b7818a3524ee273106ff28496d
Marek Czerwinski <marek.czerwinski@vimn.com>
2022-09-19 16:25:26 +0200

    Change path of the existing endpoint product(s) -> element(s)

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -5,4 +5,8 @@ router.get("/product/:id", controller.getById);
 router.get("/products", controller.getAll);
 router.get("/products2", controller.getAll);
 
+router.get("/element/:id", controller.getById);
+router.get("/elements", controller.getAll);
+router.get("/elements2", controller.getAll);
+
 module.exports = router;
