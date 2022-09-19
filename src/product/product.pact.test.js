b324251962a00172c4928b8467fb29964d50c17f
Marek Czerwinski <marek.czerwinski@vimn.com>
2022-09-19 13:23:31 +0200

    Add a new field (material) support

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -40,12 +40,12 @@ describe("Pact Verification", () => {
     const stateHandlers = {
       "products exist": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "green")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "green", "gold")]
         ]);
       },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "yellow")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "yellow", "leather")]
         ]);
       },
       "a product with ID 11 does not exist": () => {
